5fe3960ffb4d038fa620b1414f532ccc87757bc9
DavidRodriguez <davisito544@gmail.com>
2024-06-20 22:23:08 -0500

    (Update) se agrego clases para la coneccion a la base de datos

--- proyecto1/backend/app.js
+++ proyecto1/backend/app.js
@@ -1,10 +1,20 @@
+// importaciones
 import express from "express";
 import dotenv from "dotenv";
+import connectionMongo from "./config/db.js";
 
+//  configuracion del servidor
 const app = express();
 dotenv.config();
 const port = process.env.PORT;
 
+// app.get("/", (req, res) => {
+//   res.send("Hi there, this is a request to my server");
+// });
+// coneccion a la base de datos
+connectionMongo();
+
+// ejecutar el servidor
 app.listen(port, () => {
   console.log(`el servidor se esta escuchando en: http:// localhost:${port}`);
 });
---
+++ proyecto1/backend/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const connectionMongo = async () => {
+  await mongoose.connect(process.env.CONEXION_DB, {});
+  try {
+    console.log("conexion exitosa con la base de datos");
+  } catch (error) {
+    console.error("Error de conexion:", error.message);
+  }
+};
+export default connectionMongo;
