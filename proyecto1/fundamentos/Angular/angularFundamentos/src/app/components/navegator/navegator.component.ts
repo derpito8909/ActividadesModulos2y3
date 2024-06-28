986f9dcbf6c3865d6c61d5e99656bce3ef9f18b1
DavidRodriguez <davisito544@gmail.com>
2024-06-27 23:15:39 -0500

    [actualizacion] creacion del primer proyecto de angular

---
+++ proyecto1/fundamentos/Angular/angularFundamentos/src/app/components/navegator/navegator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navegator',
+  standalone: true,
+  imports: [],
+  templateUrl: './navegator.component.html',
+  styleUrl: './navegator.component.css'
+})
+export class NavegatorComponent {
+
+}
